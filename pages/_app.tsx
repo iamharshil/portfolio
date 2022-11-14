e521a0f5574be085ee0a950c29cc5639c2883d8e
iamharshil <harshilc71@gmail.com>
2022-11-14 13:23:02 +0530

    added scroll effect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import ScrollObserver from "../utils/scroll-observer";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ScrollObserver>
       <Component {...pageProps} />
-    </>
+    </ScrollObserver>
   );
 }
 
